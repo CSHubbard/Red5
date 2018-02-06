0a63db0178a3f850783b95ceb72f546693bf0cb5
CSHubbard <CodySpHubbard@gmail.com>
2018-02-05 23:00:40 -0800

    Added a bunch of gyroscope stuff

--- Android/App/app/src/main/java/nothawk/red5/MainActivity.java
+++ Android/App/app/src/main/java/nothawk/red5/MainActivity.java
@@ -1,13 +1,176 @@
 package nothawk.red5;
 
 import android.support.v7.app.AppCompatActivity;
+import android.graphics.Canvas;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+import android.util.AttributeSet;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GyroOutput mGyroOutput;
+    private SensorManager mSensorManager;
+    private Sensor mGyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get an instance of the SensorManager
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        //get an instance of teh gyroscope sensor
+        mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        // instantiate our stuff and set it as the activity's content
+        mGyroOutput = new GyroOutput(this);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        /*
+         * when the activity is resumed, we acquire a wake-lock so that the
+         * screen stays on, since the user will likely not be fiddling with the
+         * screen or buttons.
+         */
+        //mWakeLock.acquire();
+
+        // Start the simulation
+        mGyroOutput.startOutput();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        /*
+         * When the activity is paused, we make sure to stop the simulation,
+         * release our sensor resources and wake locks
+         */
+
+        // Stop the output
+        mGyroOutput.stopOutput();
+
+        // and release our wake-lock
+        //mWakeLock.release();
+    }
+
+    class GyroOutput extends FrameLayout implements SensorEventListener {
+
+        private static final float NS2S = 1.0f / 1000000000.0f;
+        private final float[] deltaRotationVector = new float[4];
+        private float timestamp;
+        private final float EPSILON = .001f;
+
+        class GyroText extends TextView{
+            //constructors
+            public GyroText(Context context) {
+                super(context);
+            }
+            public GyroText(Context context, AttributeSet attrst) {
+                super(context);
+            }
+        }
+
+        //constructor
+        public GyroOutput(Context context) {
+            super(context);
+            /**
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+            DisplayMetrics metrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            mXDpi = metrics.xdpi;
+            mYDpi = metrics.ydpi;
+            mMetersToPixelsX = mXDpi / 0.0254f;
+            mMetersToPixelsY = mYDpi / 0.0254f;
+
+            // rescale the ball so it's about 0.5 cm on screen
+            mDstWidth = (int) (sBallDiameter * mMetersToPixelsX + 0.5f);
+            mDstHeight = (int) (sBallDiameter * mMetersToPixelsY + 0.5f);
+            mParticleSystem = new ParticleSystem();
+
+            Options opts = new Options();
+            opts.inDither = true;
+            opts.inPreferredConfig = Bitmap.Config.RGB_565;
+             **/
+        }
+
+        public void startOutput() {
+            /*
+             * It is not necessary to get accelerometer events at a very high
+             * rate, by using a slower rate (SENSOR_DELAY_UI), we get an
+             * automatic low-pass filter, which "extracts" the gravity component
+             * of the acceleration. As an added benefit, we use less power and
+             * CPU resources.
+             */
+            mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+        }
+
+        public void stopOutput() {
+            mSensorManager.unregisterListener(this);
+
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // This timestep's delta rotation to be multiplied by the current rotation
+            // after computing it from the gyro sample data.
+            if (timestamp != 0) {
+                final float dT = (event.timestamp - timestamp) * NS2S;
+                // Axis of the rotation sample, not normalized yet.
+                float axisX = event.values[0];
+                float axisY = event.values[1];
+                float axisZ = event.values[2];
+
+                // Calculate the angular speed of the sample
+                float omegaMagnitude = (float)Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+                // Normalize the rotation vector if it's big enough to get the axis
+                // (that is, EPSILON should represent your maximum allowable margin of error)
+                if (omegaMagnitude > EPSILON) {
+                    axisX /= omegaMagnitude;
+                    axisY /= omegaMagnitude;
+                    axisZ /= omegaMagnitude;
+                }
+
+                // Integrate around this axis with the angular speed by the timestep
+                // in order to get a delta rotation from this sample over the timestep
+                // We will convert this axis-angle representation of the delta rotation
+                // into a quaternion before turning it into the rotation matrix.
+                float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+                float sinThetaOverTwo = (float)Math.sin(thetaOverTwo);
+                float cosThetaOverTwo = (float)Math.cos(thetaOverTwo);
+                deltaRotationVector[0] = sinThetaOverTwo * axisX;
+                deltaRotationVector[1] = sinThetaOverTwo * axisY;
+                deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+                deltaRotationVector[3] = cosThetaOverTwo;
+            }
+
+            timestamp = event.timestamp;
+            float[] deltaRotationMatrix = new float[9];
+            SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+            // User code should concatenate the delta rotation we computed with the current rotation
+            // in order to get the updated rotation.
+            // rotationCurrent = rotationCurrent * deltaRotationMatrix;
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+
+            // and make sure to redraw asap
+            invalidate();
+        }
     }
 }
