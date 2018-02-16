d9141b20fa8517eb49e9060b530bba1504f0a7a0
CSHubbard <CodySpHubbard@gmail.com>
2018-02-15 22:26:01 -0800

    Added a bunch of server communcation code. Not fucntional yet.

--- Server/index.js
+++ Server/index.js
@@ -13,3 +13,43 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+//server stuff from https://causeyourestuck.io/2016/04/27/node-js-android-tcpip/
+var net = require('net');
+var sockets = [];
+
+// creates the server (no way) and takes as a parameter a callback function called
+// every time a new client connects to the server. The client's socket is contained in sock.
+var svr = net.createServer(function(sock) {
+    console.log('Connected: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sockets.push(sock);
+
+    //sock.write('Welcome to the server!\n');
+	
+	// a callback called every time the client sends a message. 
+    sock.on('data', function(data) {
+        for (var i=0; i<sockets.length ; i++) {
+            if (sockets[i] != sock) {
+                if (sockets[i]) {
+                    sockets[i].write(data);
+                }
+            }
+        }
+    });
+	
+	// a callback called when the client disconnects from the server.
+    sock.on('end', function() {
+        console.log('Disconnected: ' + sock.remoteAddress + ':' + sock.remotePort);
+        var idx = sockets.indexOf(sock);
+        if (idx != -1) {
+            delete sockets[idx];
+        }
+    });
+});
+
+var svraddr = '192.168.0.8';
+var svrport = 1234;
+
+svr.listen(svrport, svraddr);
+console.log('Server Created at ' + svraddr + ':' + svrport + '\n');
