5c8d092fa3e986d89396383d7c9097137475234d
CSHubbard <CodySpHubbard@gmail.com>
2018-03-05 00:59:37 -0800

    What: Updated/returned server code to use express and ejs files as origianlly intended.

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,7 @@
 //Initialize Dependencies
-var app = require('express')();
+var express = require('express');
+var path = require('path');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
@@ -13,9 +15,12 @@ server.listen(port, function(){
 
 //Server send HTML file to client
 //This piece is only for testing the server with browsers
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
+//set static path
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
 
 //Action that happens when a client is connected to the server
 io.on('connection', function(client){
@@ -23,15 +28,18 @@ io.on('connection', function(client){
 	client.on('disconnect', function(){
 		console.log('Client disconnected...');
 	});
+	
 	client.on('join', function(room){
 		client.join(room);
 		console.log(room + ' client joined!');
 	});
+	
 	//Receive data from clients in "Android" room
 	client.on('pushData', function(data){
-		console.log(data); //Prints gyroscope data
-		//DO SOMETHING WITH THE DATA FROM GYROSCOPE HERE
+		console.log(data); //Prints orientation data
+		//DO SOMETHING WITH THE DATA FROM phone HERE
 	});
+	
 	//Send data to clients "Drone" room
 		//INSERT DRONE STUFF HERE
 		//io.sockets.in(Drone).emit(droneData);
